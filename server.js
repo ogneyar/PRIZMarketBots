4794e7f86d518299d09238bca244a3b211499fe3
ogneyar <ogneyar-ne@ya.ru>
2022-12-03 22:54:34 +0300

    minor edits

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
+const sendMessage = require('./prizm_market_bot/sendMessage')
+
+// const http
 
 const port = 5000
 
@@ -11,7 +14,15 @@ app.use(favicon(path.join(__dirname,'favicon.ico')))
 
 app.get('/error', (req, res) => res.status(404).sendFile(__dirname + '/404.html'))
 
-app.get('/prizm_market_bot', (req, res) => res.status(200).send('ok'))
+app.get('/prizm_market_bot', async (req, res) => {
+    let chat_id = ""
+    if (req.body && req.body.message) {
+        chat_id = req.body.message.chat.id
+    }
+    sendMessage("Бот на ремонте " + chat_id)
+    
+    res.status(200).send('ok')
+})
 
 
 app.get('*', (req, res) => res.sendFile(__dirname + '/index.html'))
